46162ca4d2fff61c5817f979626e3e045706e646
user9261 <nre12270@gmail.com>
2024-05-03 22:09:48 +0900

    url変更

--- kadai_002/urls.py
+++ kadai_002/urls.py
@@ -21,6 +21,8 @@ from accounts import views as accounts_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
+from django.urls import path, re_path
 
 
 urlpatterns = [
@@ -59,3 +61,7 @@ urlpatterns = [
 # if settings.DEBUG:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += [
+   re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+   re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
