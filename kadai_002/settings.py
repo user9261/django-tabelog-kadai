80fbe2324731c570fca66f315b9a70166ee20cfc
user9261 <nre12270@gmail.com>
2024-05-20 15:18:36 +0900

    AWS S3

--- kadai_002/settings.py
+++ kadai_002/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'storages',
 
 ]
 
@@ -181,3 +182,16 @@ AUTH_USER_MODEL = 'accounts.User'
 ACCOUNT_FORMS = {
   'signup': 'accounts.forms.CustomSignupForm'
 }
+
+#追加
+import os
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = S3_URL
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
