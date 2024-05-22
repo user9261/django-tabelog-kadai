944f3d4042f1c681e9c2f80ad2147d70756bbf1e
user9261 <nre12270@gmail.com>
2024-05-22 15:35:42 +0900

    DEBUG = False

--- kadai_002/settings.py
+++ kadai_002/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(aslu2ddv2n&*y1!1ico0i%78syk3$-+y(p=q5x)evgi!i4=35'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
